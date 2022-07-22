6f2197e0d750cb387472a2e7a47abb85fecfee53
Sarah Collison <sarah@blameless.com>
2022-07-22 12:38:15 -0700

    Do not cd into GITHUB_WORKSPACE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
 
-if [ -n "$GITHUB_WORKSPACE" ]; then
-	cd "$GITHUB_WORKSPACE"
-fi
-
 QUERY="${2:-.}"
 
 echo "::set-output name=config::$(terraform-config-inspect --json "$1" | gojq -cr $QUERY)"
